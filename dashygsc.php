8793c19ba2dfd1e9181d7b5a99bef1af35fc71c1
tropicalista <tropicalista@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-09-24 15:58:12 +0000

    fix version
    git-svn-id: https://plugins.svn.wordpress.org/search-console/trunk@1946249 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dashygsc.php
+++ dashygsc.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Search Console
  * Plugin URI:        https://tropicalseo.net/
  * Description:       This plugin displays your Google Search Console Analytics data inside your WordPress.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Tropicalista
  * Author URI:		  https://tropicalseo.net
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'DASHYGSC_VERSION', '1.0.1' );
+define( 'DASHYGSC_VERSION', '1.0.2' );
 
 /**
  * The code that runs during plugin activation.
