9ba632a31af8d2cc3cdf1cf67475495d15f862bc
tropicalista <tropicalista@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-10-11 14:50:02 +0000

    added missing js
    git-svn-id: https://plugins.svn.wordpress.org/search-console/trunk@1955231 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- searchconsole.php
+++ searchconsole.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Search Console
  * Plugin URI:        https://tropicalseo.net/
  * Description:       This plugin displays your Google Search Console Analytics data inside your WordPress.
- * Version:           1.2.0
+ * Version:           1.2.1
  * Author:            Tropicalista
  * Author URI:		  https://tropicalseo.net/
  * License:           GPL-2.0+
@@ -37,7 +37,7 @@ if ( !defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SearchConsole_VERSION', '1.2.0' );
+define( 'SearchConsole_VERSION', '1.2.1' );
 
 function sc_fs_uninstall_cleanup()
 {
